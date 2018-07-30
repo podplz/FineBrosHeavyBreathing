fef8ced14c8560cfb0956b6a319907df3f9034fd
podplz <bornholdtlasse@gmail.com>
2018-07-30 22:34:59 +0200

    Bulma

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css'
 
 class App extends Component {
   render() {
@@ -10,9 +11,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="App-intro" class="is-size-5-desktop">
           To get started, edit <code>src/App.js</code> and save to reload.LULcommitnywr3r
         </p>
+          <button className="App-button" >Test button</button>
       </div>
     );
   }
