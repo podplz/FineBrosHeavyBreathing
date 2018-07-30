f0b8accff72be94b603253e9631d876b2a0f6327
podplz <bornholdtlasse@gmail.com>
2018-07-30 22:15:44 +0200

    LUL3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.LUL
+          To get started, edit <code>src/App.js</code> and save to reload.LULcommitnywr3r
         </p>
       </div>
     );
