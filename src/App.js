1104874c4178d11fec3885aab1b03b41fda81bb0
TheHackerKnownAs4Chan <knash15@student.aau.dk>
2018-08-02 00:58:35 +0200

    Some design

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class App extends Component {
   render() {
+      function handleClick(e) {
+          e.preventDefault();
+          alert("Logan paul");
+
+      }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        </p>
-          <button className="App-button" >Test button</button>
+          <button className="App-button button is-primary" onClick={handleClick}>Test button</button>
+          <div className="tabs is-right is-boxed">
+              <ul>
+                  <li className="is-active"><a>Pictures</a></li>
+                  <li><a><span className="icon is-large"><FontAwesomeIcon icon={["fab","instagram"]} /></span></a>
+                  <span>Camwhores</span>
+                  </li>
+                  <li><a>Videos</a></li>
+                  <li><a>Documents</a></li>
+              </ul>
+          </div>
       </div>
     );
   }
---
+++ src/test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kristoffer
+ * Date: 01-08-2018
+ * Time: 20:35
+ */
